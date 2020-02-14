35db9229b85c430d0a03a4063eff76be5b0bdf7a
Geovani de Souza <geovanisouza92@gmail.com>
2020-02-14 19:56:27 -0300

    Enhance logging format
    Signed-off-by: Geovani de Souza <geovanisouza92@gmail.com>

--- src/infrastructure/logging/index.ts
+++ src/infrastructure/logging/index.ts
@@ -1,24 +1,56 @@
 import { asFunction, AwilixContainer } from 'awilix';
-import { createLogger as winstonCreateLogger, format, Logger, transports } from 'winston';
+import {
+  createLogger as winstonCreateLogger,
+  format,
+  LeveledLogMethod,
+  LogEntry,
+  LogMethod,
+  transports,
+} from 'winston';
 import { Environment } from '../../../config/environment';
+import { currentRequestId } from '../../../lib/request-id';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const customFormat = format.printf(({ level, message, timestamp }: any): string => {
-  return `${timestamp} ${level}: ${message}`;
+export interface Logger {
+  log: LogMethod;
+  error: LeveledLogMethod;
+  warn: LeveledLogMethod;
+  info: LeveledLogMethod;
+  debug: LeveledLogMethod;
+}
+
+const addRequestId = format((info) => {
+  const requestId = currentRequestId();
+  if (requestId) {
+    info['request-id'] = requestId;
+  }
+  return info;
 });
 
 function createLogger({ environment }: { environment: Environment }): Logger {
-  const logger = winstonCreateLogger({
+  const formats = [addRequestId(), format.timestamp()];
+
+  if (environment.logger.format === 'json') {
+    formats.push(format.json());
+  } else {
+    formats.push(
+      format.colorize(),
+      format.printf((entry: LogEntry) => {
+        const { timestamp, level, message, ...rawMeta } = entry;
+        const meta = JSON.stringify(rawMeta, null, 2);
+        return `${timestamp} [${level}]: ${message.trim()} ${meta}`;
+      }),
+    );
+  }
+
+  return winstonCreateLogger({
     level: environment.logger.level,
-    format: format.combine(format.timestamp(), format.colorize(), customFormat),
+    format: format.combine(...formats),
     transports: [new transports.Console()],
   });
-
-  return logger;
 }
 
 export function mountLoggingModule(container: AwilixContainer): void {
   container.register({
-    logger: asFunction(createLogger).singleton(),
+    logger: asFunction(createLogger).scoped(),
   });
 }
