9b97fbb8b80c117b4cb84d860ae349e7a683497b
Geovani de Souza <geovanisouza92@gmail.com>
2020-02-14 20:11:06 -0300

    Add example uses
    Signed-off-by: Geovani de Souza <geovanisouza92@gmail.com>

--- src/components/healthcheck/use-cases/check-liveness.ts
+++ src/components/healthcheck/use-cases/check-liveness.ts
@@ -1,8 +1,17 @@
+import { Inject, Injectable } from '../../../../lib/container';
 import { UseCase } from '../../../../lib/use-case';
+import { UserRepository } from '../../authentication/infrastructure/user-repository';
 import { StatusReport } from '../domain/status-report';
 
+@Injectable
 export class CheckLivenessUseCase implements UseCase<void, StatusReport> {
+  @Inject
+  private userRepository!: UserRepository;
+
   async execute(): Promise<StatusReport> {
-    return { status: 'ok' };
+    // TODO Wrong
+    const howManyUsers = await this.userRepository.count();
+
+    return { status: 'ok', howManyUsers: String(howManyUsers) };
   }
 }
