dc439a963c94dca9b616f7e62d05be48c454df2b
Geovani de Souza <geovanisouza92@gmail.com>
2020-02-29 10:02:08 -0300

    usa injeção no database logger
    Signed-off-by: Geovani de Souza <geovanisouza92@gmail.com>

--- src/infrastructure/database/database-logger.ts
+++ src/infrastructure/database/database-logger.ts
@@ -1,39 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { AwilixContainer } from 'awilix';
 import { Logger as typeormLogger } from 'typeorm';
+import { Inject, Injectable } from '../../../lib/container';
 import { Logger } from '../logging';
 
+@Injectable
 export class DatabaseLogger implements typeormLogger {
-  constructor(private container: AwilixContainer) {}
+  @Inject
+  private logger!: Logger;
 
   logQuery(query: string, parameters?: any[]): void {
-    const logger = this.container.resolve<Logger>('logger');
-    logger.info(`QUERY: ${query}`, { parameters });
+    this.logger.info(`QUERY: ${query}`, { parameters });
   }
 
   logQueryError(error: string, query: string, parameters?: any[]): void {
-    const logger = this.container.resolve<Logger>('logger');
-    logger.error(`SQL ERROR FOR QUERY: "${query}"`, { parameters, error });
+    this.logger.error(`SQL ERROR FOR QUERY: "${query}"`, { parameters, error });
   }
 
   logQuerySlow(time: number, query: string, parameters?: any[]): void {
-    const logger = this.container.resolve<Logger>('logger');
-    logger.warn(`SLOW QUERY TOOK [${time}]: "${query}"`, { parameters });
+    this.logger.warn(`SLOW QUERY TOOK [${time}]: "${query}"`, { parameters });
   }
 
   logSchemaBuild(message: string): void {
-    const logger = this.container.resolve<Logger>('logger');
-    logger.debug(message);
+    this.logger.debug(message);
   }
 
   logMigration(message: string): void {
-    const logger = this.container.resolve<Logger>('logger');
-    logger.debug(message);
+    this.logger.debug(message);
   }
 
   log(level: 'log' | 'info' | 'warn', message: any): void {
-    const logger = this.container.resolve<Logger>('logger');
-    logger.log(level, message);
+    this.logger.log(level, message);
   }
 }
--- src/infrastructure/database/index.ts
+++ src/infrastructure/database/index.ts
@@ -1,13 +1,13 @@
-import { asClass, asValue, AwilixContainer } from 'awilix';
+import { asClass, AwilixContainer } from 'awilix';
+import { mountUserRepository } from '../../components/authentication/infrastructure/repositories';
 import { Database } from './database';
 import { DatabaseLogger } from './database-logger';
-import { mountUserRepository } from '../../components/authentication/infrastructure/repositories';
 
 export { openTransaction } from './middlewares/open-transaction';
 
 export function mountDatabaseModule(container: AwilixContainer): void {
   container.register({
-    databaseLogger: asValue(new DatabaseLogger(container)),
+    databaseLogger: asClass(DatabaseLogger),
     database: asClass(Database)
       .singleton()
       .disposer((database) => database.close()),
